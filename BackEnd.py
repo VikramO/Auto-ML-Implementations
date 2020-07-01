7ead77bf047feb350928494782e7d3c53957fefa
NDevanathan <nikhil.devanathan@gmail.com>
2020-06-30 18:36:16 -0700

    Create BackEnd.py
    ndev 6/30/2020:
    Created BackEnd.py to serve as the back-end, ML-implementation file.

---
+++ BackEnd.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 30 2020
+Updated on Tue Jun 30 2020
+
+Python file to handle training, testing, and utilization of ML models.
+Eventually, cloud computation resources will be used, but local resources
+will be used for current development phase.
+"""
+
+import keras
+import cython
+
+class CNN():
+    
+
+class RNN():
+    
