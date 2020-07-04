125bbbab09eb9589c75529f4510dc86dba3b48f6
VikramO <vikramoruganti@gmail.com>
2020-07-04 03:02:50 -0400

    Update AutoKerasImplemntation.py

--- AutoKerasImplemntation.py
+++ AutoKerasImplemntation.py
@@ -380,5 +380,5 @@ class AutoARIMA():
         
         error = smape(y_test, forecasts)
         mae = mean_absolute_error(y_test, forecasts)
-        print("Symmetric Absolute Percentage Error: ", error)
+        print("Symmetric Mean Absolute Percentage Error: ", error)
         print("Mean Absolute Error: ", mae)
